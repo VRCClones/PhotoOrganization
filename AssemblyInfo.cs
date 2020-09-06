016d58fc772de0febb07df5f37a550a971030257
dave-kun <66936858+dave-kun@users.noreply.github.com>
2020-09-06 12:48:29 +1000

    Update Melon assembly attributes.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -16,10 +16,10 @@ using MelonLoader;
 [assembly: AssemblyVersion(PhotoOrganization.BuildInfo.Version)]
 [assembly: AssemblyFileVersion(PhotoOrganization.BuildInfo.Version)]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: MelonModInfo(typeof(PhotoOrganization.PhotoOrganization), PhotoOrganization.BuildInfo.Name, PhotoOrganization.BuildInfo.Version, PhotoOrganization.BuildInfo.Author, PhotoOrganization.BuildInfo.DownloadLink)]
+[assembly: MelonInfo(typeof(PhotoOrganization.PhotoOrganization), PhotoOrganization.BuildInfo.Name, PhotoOrganization.BuildInfo.Version, PhotoOrganization.BuildInfo.Author, PhotoOrganization.BuildInfo.DownloadLink)]
 
 
 // Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
 // If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
 // Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
-[assembly: MelonModGame(null, null)]
+[assembly: MelonGame("VRChat", "VRChat")]
